baf3f4dc22063bfb2bb1f11675b1ffba5d0eddb6
Ashish Lal <ridininmybenz44@gmail.com>
2015-05-12 23:41:01 -0700

    Created zip file

--- sql/drop.sql
+++
@@ -1,9 +0,0 @@
-drop table MaxMovieID;
-drop table MaxPersonID;
-drop table Review;
-drop table MovieActor;
-drop table MovieDirector;
-drop table MovieGenre;
-drop table Director;
-drop table Actor;
-drop table Movie;
