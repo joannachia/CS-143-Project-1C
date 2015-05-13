7540c85e09eba09e0e920dbe2cefafa110867bb7
Ashish Lal <ridininmybenz44@gmail.com>
2015-05-12 21:21:03 -0700

    Changed header

--- header.php
+++ header.php
@@ -3,41 +3,27 @@
 <head><title>CS143 Project 1C Demo</title></head>
 
 <body>
-<table>
-
-
-<!-- Left side of page -->
-<tr>
 <h2>Navigation</h2>
-</tr>
 
-<tr>
-<td>
-Add: 
-<ul>
-	<li><a href="add_actor.php" target="main">Actor</a></li>
-	<li><a href="add_movie.php" target="main">Movie</a></li>
-	<li><a href="add_actor_movie.php" target="main">Actor to Movie</a></li>
-</ul>
-</td>
-<td>
-Browsing:
-<ul>
-	<li><a href="b_actor.php" target="main">By Actor</a></li>
-	<li><a href="b_movie.php" target="main">By Movies</a></li>
-</ul>
+<table style="width:50%">
+  <tr>
+    <th style="text-align:left">Add:</th>
+    <th style="text-align:left">Browsing:</th>
+    <th style="text-align:left">Search:</th>
+  </tr>
+  <tr>
+    <td style="text-align:left"><a href="add_actor.php" target="main">Actor</a></td>
+    <td><a href="b_actor.php" target="main">By Actor</a></td>
+    <td><a href="search.php" target="main">Actors or Movies</a></td>
+  </tr>
+  <tr>
+    <td><a href="add_movie.php" target="main">Movie</a></td>
+    <td><a href="b_movie.php" target="main">By Movies</a></td>
+  </tr>
+  <tr>
+    <td><a href="add_actor_movie.php" target="main">Actor to Movie</a></td>
+  </tr>
+</table>
 
-</td>
-<td>
-
-Search:
-<ul>
-	<li><a href="search.php" target="main">Actors or Movies</a></li>
-
-</ul>
-</td>
-
-</tr>
-
-
-</td>
+</body>
+</html>
